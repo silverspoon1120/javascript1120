571dff78de9e302b62e16a9ac424d6ce050d0403
Silver Spoon <silverspoon1120.sw@gmail.com>
2020-10-19 08:49:33 -0300

    Update README.md

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,36 +8,28 @@ require('dotenv').config({
     path: envPath
 });
 
-/////////////////////////////////////
-
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const { errors } = require('celebrate');
-const http = require('http');
-const socketio = require('socket.io');
-
-require('./database/connection');
 
 const trimBody = require('./middlewares/trimBody');
-const { socketConnection } = require('./websocket/socketConnection');
+
 const routes = require('./routes');
+require('./database/connection');
 
 const app = express();
-const server = http.createServer(app);
-const sIo = socketio(server);
-
+ 
 app.use(cors({ origin: process.env.CORS_ORIGIN_URL }));
 app.use(express.json());
 //app.use(express.urlencoded({ extended: true }));
 
 app.use(trimBody);
 
-socketConnection(sIo);
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.use(errors());
     
-module.exports = server;
+module.exports = app;
--- backend/src/controllers/orderAdminController/index.js
+++
@@ -1,9 +0,0 @@
-const list = require('./list');
-const update = require('./update');
-const destroy = require('./destroy');
-
-module.exports = {
-    list,
-    update,
-    destroy
-}
