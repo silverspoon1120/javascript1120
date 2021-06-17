20d7dc72eccc569106f201427e37a44e03220e0f
Silver Spoon <silverspoon1120.sw@gmail.com>
2021-06-17 13:23:13 -0300

    refactor: set default value

--- ts-backend/src/controllers/userController/validators/destroy.ts
+++
@@ -1,8 +0,0 @@
-import { celebrate, Segments, Joi } from 'celebrate';
-
-export default celebrate({
-    [Segments.HEADERS]: Joi.object().keys({
-        authorization: Joi.string().required()
-    })
-    .unknown(),
-});
